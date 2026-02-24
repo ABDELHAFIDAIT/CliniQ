399cbbe232fecebb3abc5b33c5faec4c6a55e121
ABDELHAFIDAIT <abdelhafid02002@gmail.com>
2026-02-24 15:45:09 +0000

    feat(core): implémentation de la gestion des paramètres globaux avec Pydantic Settings

---
+++ app/core/config.py
@@ -0,0 +1,44 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    PROJECT_NAME : str
+    API_STR : str
+    VERSION : str
+    
+    POSTGRES_USER : str
+    POSTGRES_PASSWORD : str
+    POSTGRES_DB : str
+    POSTGRES_HOST : str
+    POSTGRES_PORT : int
+    
+    DATABASE_URL : str
+    
+    JWT_SECRET_KEY : str
+    JWT_ALGORITHM : str
+    JWT_ACCESS_TOKEN_EXPIRE_MINUTES : int
+    
+    LLM_MODEL : str
+    EMBEDDING_MODEL : str
+    GEMINI_API_KEY : str
+    
+    QDRANT_URL : str
+    QDRANT_COLLECTION_NAME : str
+    
+    CHUNK_SIZE : int
+    CHUNK_OVERLAP : int
+    RETRIEVAL_TOP_K : int
+    
+    MLFLOW_TRACKING_URI : str
+    MLFLOW_EXPERIMENT_NAME : str
+    
+    GRAFANA_ADMIN_PASSWORD : str
+    
+    model_config = SettingsConfigDict(
+        env_file=".env", 
+        case_sensitive=True,
+        extra="ignore"
+    )
+    
+    
+settings = Settings()
